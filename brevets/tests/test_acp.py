b2fd5d894daec77ec9e13df3240c219bc9e0e940
Benjamin Michalisko <bmichali@uoregon.edu>
2017-10-23 21:13:56 -0700

    edited tests

--- brevets/tests/test_acp.py
+++ brevets/tests/test_acp.py
@@ -1,12 +1,6 @@
-"""
-Nose tests for jumble.py
 
-We cannot test for randomness here (no effective oracle),
-but we can test that the elements in the returned string
-are correct.
-"""
 
-from jumble import jumbled
+from acp_times import open_time, close_time
 
 import nose    # Testing framework
 import logging
@@ -15,21 +9,33 @@ logging.basicConfig(format='%(levelname)s:%(message)s',
 log = logging.getLogger(__name__)
 
 
-def same(s, t):
-    """
-    Same characters (possibly in different order)
-    in two strings s and t.
-    """
-    return sorted(s) == sorted(t)
+def test_zero_km(s, t):
+  	#open_time(control_dist_km, brevet_dist_km, brevet_start_time)
 
+    assert open_time(0, 200, "2017-01-01T00:00:00") == "2017-01-01T00:00:00"
+    assert close_time(0, 200, "2017-01-01T00:00:00") == "2017-01-01T09:00:00+00:00"
 
-def test_jumbled_single():
-    assert same(jumbled(["abbcd"], 1), "abbcd")
+def test_200km():
+    assert open_time(200, 200, "2017-01-01T00:00:00") == "2017-01-01T13:52:56.470588+00:00"
+    assert close_time(200, 200, "2017-01-01T00:00:00") == "2017-01-01T21:30:00+00:00"
 
+def test_205km():
+    assert open_time(205, 200, "2017-01-01T00:00:00") == "2017-01-01T13:52:56.470588+00:00"
+    assert close_time(205, 200, "2017-01-01T00:00:00") == "2017-01-01T21:40:00+00:00"
 
-def test_jumbled_pair():
-    assert same(jumbled(["abbc", "abcc"], 2), "abbcc")
+def test_300km_for_200_dist():
+   	assert open_time(300, 200, "2017-01-01T00:00:00") == "2017-01-01T17:00:26.470588+00:00"
+    assert close_time(300, 200, "2017-01-01T00:00:00") == "2017-01-01T21:30:00+00:00"
 
+def test_boundaries():
+   	assert open_time(300, 300, "2017-01-01T00:00:00") == "2017-01-01T16:49:24.705882+00:00"
+    assert close_time(300, 300, "2017-01-01T00:00:00") == "2017-01-02T04:00:00+00:00"
 
-def test_jumbled_more():
-    assert same(jumbled(["aabc", "abac", "bcaa"], 2), "aabc")
+    assert open_time(400, 400, "2017-01-01T00:00:00") == "2017-01-01T19:45:52.941176+00:00"
+    assert close_time(400, 400, "2017-01-01T00:00:00") == "2017-01-02T10:40:00+00:00"
+
+    assert open_time(600, 600, "2017-01-01T00:00:00") == "2017-01-02T01:38:49.411765+00:00"
+    assert close_time(600, 600, "2017-01-01T00:00:00") == "2017-01-03T00:00:00+00:00"
+
+    assert open_time(1000, 1000, "2017-01-01T00:00:00") == "2017-01-02T13:24:42.352941+00:00"
+    assert close_time(1000, 1000, "2017-01-01T00:00:00") == "2017-01-04T02:40:00+00:00"
